386acd42c70deaee23ce75e6a9f4fd2059b9f043
miclay0260 <79045167+miclay0260@users.noreply.github.com>
2021-02-14 19:55:08 +0400

    Add files via upload

--- server.py
+++ server.py
@@ -1,21 +1,22 @@
 import socket
 
 sock = socket.socket()
-sock.bind(('', 3555))
-sock.listen(1)
+sock.bind(('', 8080))
+sock.listen(3)
 conn, addr = sock.accept()
 
 print ('connected:', addr)
 while True:
     try:
         comm = input("-> ")
-        if comm == "refuse":
+        if comm != "refuse":
             conn.send(comm.encode())  # отправляем команду
             data = conn.recv(1024).decode()  # получаем результат
             print (data)
         else:
             print ("Refused")
             conn.close()
+            exit()
     except:
          break
 print("Connection refused")  # в случае, если соединение разорванно
