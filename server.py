f80ac6cfa21f596c85227794bb7c570c3e1ea916
miclay0260 <79045167+miclay0260@users.noreply.github.com>
2021-02-14 17:05:37 +0400

    Add files via upload

--- server.py
+++ server.py
@@ -9,13 +9,9 @@ print ('connected:', addr)
 while True:
     try:
         comm = input("-> ")
-        if comm == "refuse":
-            conn.send(comm.encode())
-            conn.close()
-        else:
-             conn.send(comm.encode())  # отправляем команду
-             data = conn.recv(1024).decode()  # получаем результат
-             print (data)
+        conn.send(comm.encode())  # отправляем команду
+        data = conn.recv(1024).decode()  # получаем результат
+        print (data)
     except:
          break
 print("Connection refused")  # в случае, если соединение разорванно
